92f209a903dccdfd8930bbf2c1e30eacb521d2e1
remotecraftr <remotecraftr@users.noreply.github.com>
2023-03-17 18:34:30 -0700

    fix(db): fix database connection leak

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,4 @@
 # feat(db): setup database connection with Prisma/TypeORM
 # fix(db): fix database connection leak
 # feat(db): setup database connection with Prisma/TypeORM
+# fix(db): fix database connection leak
